3c53c25cd1cf6ff759545e6f9ff153c1fa4dbe6c
marcopolello <marco-pol@hotmail.it>
2020-11-17 18:40:45 +0100

    aggiunto intervallo per cambio slide

--- js/script.js
+++ js/script.js
@@ -2,24 +2,38 @@ var app = new Vue ({
   el: '#app',
   data: {
     pictures: ["img/image1.jpg", "img/image2.jpg", "img/image3.jpg", "img/image4.jpg"],
-    counter: 0
+    timer: null,
+    currentIndex: 0,
+    myClass: "active",
+    circleCurrent: false
+  },
+  mounted: function() {
+    this.startSlide();
   },
   methods: {
-    nextPic: function functionName() {
-      if (this.counter >= (this.pictures.length - 1)) {
-        this.counter = 0;
+    startSlide: function() {
+      this.timer = setInterval(this.nextPic, 4000);
+    },
+    nextPic: function () {
+      if (this.currentIndex >= (this.pictures.length - 1)) {
+        this.currentIndex = 0;
       } else {
-        this.counter ++;
-        // console.log(this.counter);
+        this.currentIndex ++;
+        // console.log(this.currentIndex);
       }
     },
-    prevPic: function functionName() {
-      if (this.counter === 0) {
-        this.counter = this.pictures.length - 1;
+    prevPic: function () {
+      if (this.currentIndex === 0) {
+        this.currentIndex = this.pictures.length - 1;
       } else {
-        this.counter --;
-        console.log(this.counter);
+        this.currentIndex --;
+        // console.log(this.counter);
       }
     }
-  }
+  },
+  // computed: {
+  //   currentCircle: function() {
+  //     return this.pictures[Math.abs(this.currentIndex) % this.pictures.length];
+  //   }
+  // }
 })
