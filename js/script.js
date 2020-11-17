8b2323e1e8f82b7312362c78e9b6a4e474089e43
marcopolello <marco-pol@hotmail.it>
2020-11-17 16:32:17 +0100

    nextPic al click dell'icona right

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    pictures: ["img/image1.jpg", "img/image2.jpg", "img/image3.jpg", "img/image4.jpg"],
+    counter: 0
+  },
+  methods: {
+    nextPic: function functionName() {
+      if (this.counter >= (this.pictures.length - 1)) {
+        this.counter = 0;
+      } else {
+        this.counter ++;
+        // console.log(this.counter);
+      }
+    }
+  }
+})
